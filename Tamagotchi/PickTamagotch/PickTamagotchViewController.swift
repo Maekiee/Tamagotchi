48cadc50681ed285ca7ae4718b5c65119f3250bb
Maekiee <pdwssf@gmail.com>
2025-08-22 18:15:02 +0900

    Initial Commit

---
+++ Tamagotchi/PickTamagotch/PickTamagotchViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+class PickTamagotchViewController: UIViewController {
+    let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configHeiracy()
+        configLayout()
+        configView()
+
+        
+    }
+    
+}
+
+extension PickTamagotchViewController {
+    
+    private func configHeiracy() {
+        
+    }
+    
+    private func configLayout() {
+        
+    }
+    
+    private func configView() {
+        view.backgroundColor = .white
+    }
+    
+    
+}
---
+++ Tamagotchi/PickTamagotch/PickTamagotchViewModel.swift
@@ -0,0 +1,23 @@
+import Foundation
+import RxSwift
+import RxCocoa
+
+
+class PickTamagotchViewModel {
+    let disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    init() { }
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+}
