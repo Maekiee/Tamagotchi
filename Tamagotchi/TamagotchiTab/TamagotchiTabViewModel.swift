1ac0c5ac5345150bcf9e7e5e8f0429743dacd47d
Maekiee <pdwssf@gmail.com>
2025-08-23 13:29:13 +0900

    다마고치 메인 화면 뷰모델 생성

---
+++ Tamagotchi/TamagotchiTab/TamagotchiTabViewModel.swift
@@ -0,0 +1,24 @@
+import Foundation
+import RxSwift
+import RxCocoa
+
+
+class TamagotchiTabViewModel {
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    init() { }
+
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+}
